dabbe355699e1519b510939367385d717e4a611b
Seth Lachman <lachmanseth17@gmail.com>
2019-09-08 16:34:20 -0400

    Added completed date to todo.:

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Todo = ({ todo, toggleComplete }) => {
-  return <li onClick={() => toggleComplete(todo.id)}>{todo.item}</li>;
+  return (
+    <li onClick={() => toggleComplete(todo.id)}>
+      {todo.item}{" "}
+      {todo.completed ? `Completed ${new Date().toLocaleDateString()}` : null}
+    </li>
+  );
 };
 
 export default Todo;
