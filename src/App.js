857535bffcb7e45bd22406e3bab9a29adf61e779
Seth Lachman <lachmanseth17@gmail.com>
2019-09-08 15:50:55 -0400

    Added basic setup with reducer and todolist component.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import TodoList from "./components/TodoList";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1>Todo App</h1>
+      <TodoList />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React, { useReducer } from "react";
+
+const TodoList = () => {
+  return (
+    <div>
+      <h2>Current Todos</h2>
+    </div>
+  );
+};
+
+export default TodoList;
