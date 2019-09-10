5035b328b07e0da060a7b6d30801569df2c8f0b2
Seth Lachman <lachmanseth17@gmail.com>
2019-09-09 22:01:29 -0400

    Added strike through to text.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,16 +5,51 @@ const CompletedText = styled.span`
   margin-left: 1.5rem;
 `;
 
+const StrikeThrough = styled.span`
+  position: relative;
+  @keyframes strike {
+    0% {
+      width: 0;
+    }
+    100% {
+      width: 100%;
+    }
+  }
+
+  ${props => {
+    if (props.completed) {
+      return `
+      &:after {
+        content: ' ';
+        position: absolute;
+        top: 50%;
+        left: 0;
+        width: 100%;
+        height: 1px;
+        background: black;
+        animation-name: strike;
+        animation-duration: 0.5s;
+        animation-timing-function: linear;
+        animation-iteration-count: 1;
+        animation-fill-mode: forwards; 
+      }
+      `;
+    }
+  }}
+`;
+
 const Todo = ({ todo, toggleComplete }) => {
   return (
     <li onClick={() => toggleComplete(todo.id)}>
-      {todo.item} *{todo.tag}
-      {""}
-      {todo.completed ? (
-        <CompletedText>
-          {`Completed ${new Date().toLocaleDateString()}`}
-        </CompletedText>
-      ) : null}
+      <StrikeThrough completed={todo.completed}>
+        {todo.item} *{todo.tag}
+        {""}
+        {todo.completed ? (
+          <CompletedText>
+            {`Completed ${new Date().toLocaleDateString()}`}
+          </CompletedText>
+        ) : null}
+      </StrikeThrough>
     </li>
   );
 };
