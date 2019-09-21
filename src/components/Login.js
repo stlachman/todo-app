769d6b25149444e950931f9fc6388320627b31b8
Seth Lachman <lachmanseth17@gmail.com>
2019-09-21 16:47:49 -0400

    Added login functionality.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const Login = () => {
+const Login = props => {
+  const [credentials, setCredentials] = useState({
+    name: "",
+    password: ""
+  });
+
+  const handleChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post(`/login`, credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.token);
+        props.history.push("/tasks");
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    setCredentials({ name: "", password: "" });
+  };
+
   return (
     <div>
       <h2>Login Below</h2>
-      <form>
-        <label htmlFor="username">Username</label>
-        <input type="text" name="username" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          onChange={handleChange}
+          value={credentials.name}
+          type="text"
+          name="name"
+        />
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" />
+        <input
+          onChange={handleChange}
+          value={credentials.password}
+          type="password"
+          name="password"
+        />
+        <button type="submit">Login</button>
       </form>
     </div>
   );
